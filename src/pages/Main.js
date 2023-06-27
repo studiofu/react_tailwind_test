761e119d690fa552981c399d6f1747d7e4ede22f
Johnson Fu <johnson.fu@gmail.com>
2023-06-27 10:49:09 +0800

    use material ui

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,11 @@
+import Button from '@mui/material/Button';
 
 const Main = () => {
 
     return (
         <div>
             <h1>Main</h1>
+            <Button variant="contained">Hello World</Button>
         </div>
     )
 }

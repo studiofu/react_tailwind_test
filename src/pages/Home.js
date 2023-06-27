755c8102cb09815c93becbd16c144abd73db5f30
Johnson Fu <johnson.fu@gmail.com>
2023-06-27 10:43:47 +0800

    routing update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,22 @@
 const Home = () => {
     return (
         <div className="
+            flex
             w-20 h-20 bg-red-500
             content-center
+            justify-center
+            items-center
             text-red-200
+            select-none
         ">
-            <h1>Home</h1>
+            <h1 className="
+                cursor-pointer
+                hover:bg-red-400
+                bg-red-600
+                rounded-md
+                p-1
+                m-1
+            ">Home</h1>
         </div>
     );
 }
